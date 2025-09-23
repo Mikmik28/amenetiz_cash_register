cf31f8d3d8d009634d3c1491af47b3a66d5b5b85
Mikee Agregado <mikeeagregado28@gmail.com>
2025-09-23 10:42:13 +0800

    Add qty validation

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,7 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  # Validations
+  validates :qty, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 end
