41e77025c8235d3b41134c834f480af21fd59102
Mikee Agregado <mikeeagregado28@gmail.com>
2025-09-23 11:42:33 +0800

    Fix rubocop warnings

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   # Money integration
   monetize :amount_cents
-  
+
   # Validations
   validates :code, presence: true, uniqueness: true
   validates :amount_cents, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- config/initializers/money.rb
+++ config/initializers/money.rb
@@ -1,7 +1,7 @@
 MoneyRails.configure do |config|
   config.default_currency = :eur
   config.no_cents_if_whole = false
-  config.amount_column = { postfix: '_cents' }
+  config.amount_column = { postfix: "_cents" }
   config.locale_backend = :i18n
   I18n.locale = :en
   config.rounding_mode = BigDecimal::ROUND_HALF_UP
